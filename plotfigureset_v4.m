b8ef72c0526621a5f187346a77eb434e0340f697
ki-kyeong <ooyae93@gmail.com>
2023-06-02 16:25:32 +0900

    Update plotfigureset_v4.m
    align용 데이터에서는 spectrum을 안그리도록 만들었음

--- plotfigureset_v4.m
+++ plotfigureset_v4.m
@@ -3,33 +3,36 @@ clf;
 
 % savename = data.savename;
 
-figure('Name',data.savename+" abs spec")
-plotspec_v4(gca, data, 'abs', band);
-saveas(gcf, './K_results/'+data.savename+'_abs_spectrum.png');
-saveas(gcf, './K_results/'+data.savename+'_abs_spectrum.fig');
+if data.size.freq ~= 1
+
+    figure('Name',data.savename+" abs spec")
+    plotspec_v4(gca, data, 'abs', band);
+    saveas(gcf, './K_results/'+data.savename+'_abs_spectrum.png');
+    saveas(gcf, './K_results/'+data.savename+'_abs_spectrum.fig');
 
 
-figure('Name',data.savename+" fl spec")
-plotspec_v4(gca, data, 'fl1', band);
-saveas(gcf, './K_results/'+data.savename+'_fl_spectrum.png');
-saveas(gcf, './K_results/'+data.savename+'_fl_spectrum.fig');
-% 
-% 
-% figure('Name',results.savename+" fl spec 2")
-% plotspec_v3(gca, results, 'fl2', band);
-% saveas(gcf, './K_results/'+results.savename+'_fl_spectrum-2.png');
-% saveas(gcf, './K_results/'+results.savename+'_fl_spectrum-2.fig');
-% 
-% 
-% fig=figure('Name',results.savename+" absfl spec")
-% fig.Position = [120 130 2*560 1*420]
-% tiledlayout(1,2)
-% plotspec_v3(nexttile, results, 'abs', band)
-% % plotspec_v2(nexttile, results, 'fl1')
-% plotspec_v2(nexttile, results, 'fl2')
-% saveas(gcf, './K_results/'+results.savename+'_absfl_spectrum.png');
-% saveas(gcf, './K_results/'+results.savename+'_absfl_spectrum.fig');
-% 
+    figure('Name',data.savename+" fl spec")
+    plotspec_v4(gca, data, 'fl1', band);
+    saveas(gcf, './K_results/'+data.savename+'_fl_spectrum.png');
+    saveas(gcf, './K_results/'+data.savename+'_fl_spectrum.fig');
+    %
+    %
+    % figure('Name',results.savename+" fl spec 2")
+    % plotspec_v3(gca, results, 'fl2', band);
+    % saveas(gcf, './K_results/'+results.savename+'_fl_spectrum-2.png');
+    % saveas(gcf, './K_results/'+results.savename+'_fl_spectrum-2.fig');
+    %
+    %
+    % fig=figure('Name',results.savename+" absfl spec")
+    % fig.Position = [120 130 2*560 1*420]
+    % tiledlayout(1,2)
+    % plotspec_v3(nexttile, results, 'abs', band)
+    % % plotspec_v2(nexttile, results, 'fl1')
+    % plotspec_v2(nexttile, results, 'fl2')
+    % saveas(gcf, './K_results/'+results.savename+'_absfl_spectrum.png');
+    % saveas(gcf, './K_results/'+results.savename+'_absfl_spectrum.fig');
+    %
+end
 % 
 figure('Name',data.savename+" abs 2D");
 plot2Dtimedet_v3(gca, data, 'abs');
